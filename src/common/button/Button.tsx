d2c9b85c828e409f9ba16857d9ee12033ce710d5
jezeony <jezeony@gmail.com>
2024-11-18 19:16:15 +0900

    feat : 버튼 공통 컴포넌트 수정

--- src/common/button/Button.tsx
+++ src/common/button/Button.tsx
@@ -1,19 +1,20 @@
 type TProps = {
     children: React.ReactNode;
-    mode?: "default" |'outline';
+	mode?: "default" | 'outline';
+	className?: string;
 };
 
-export default function Button({ children, mode='default' }: TProps) {
+export default function Button({ children, mode='default',className }: TProps) {
 
 
-    const borderColor = mode === "outline" ? "border-gray-600" : "border-transparent";
-    const txtColor = mode === "outline" ? "text-gray-600" : "text-white";
+    const borderColor = mode === "outline" ? "border-gray-400" : "border-transparent";
+    const txtColor = mode === "outline" ? "text-gray-400" : "text-white";
     const bgColor = mode==="outline" ?  "bg-white" :"bg-cyan-500";
 
 
 	return (
 		<button
-			className={`w-22 h-10 whitespace-nowrap border border-solid px-4 leading-9 rounded text-sm ${borderColor} ${txtColor} ${bgColor}`}
+			className={`w-22 h-10 whitespace-nowrap border border-solid px-4 leading-9 rounded-lg text-sm ${borderColor} ${txtColor} ${bgColor} ${className}`}
 		>
 			{children}
 		</button>
