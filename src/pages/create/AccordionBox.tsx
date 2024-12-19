27d2d7cf5e202c2f75120c6b6c2ec49d5490dca4
jezeony <jezeony@gmail.com>
2024-12-19 16:05:11 +0900

    feat : 신랑측, 신부측 정보 입력란 추가

--- src/pages/create/AccordionBox.tsx
+++ src/pages/create/AccordionBox.tsx
@@ -1,20 +1,26 @@
 import { ArrowDown, ArrowUp } from "@/assets/svg";
 import useModal from "@/hooks/useModal";
 
-export default function AccordionBox() {
+type TProps = {
+	title: string;
+	children: React.ReactNode;
+};
+
+export default function AccordionBox({ title, children }: TProps) {
 	const [isOpen, handleToggle] = useModal();
 
 	return (
 		<div>
 			<button
 				onClick={handleToggle}
-				className="flex justify-between items-center gap-4 px-5 py-4  font-semibold  bg-white  border border-gray-200 w-full">
-				<p>테마</p>
+				className="flex justify-between items-center gap-4 px-5 py-4  font-semibold  bg-white  border border-gray-200 w-full"
+			>
+				<p>{title}</p>
 
 				{isOpen ? <ArrowUp /> : <ArrowDown />}
 			</button>
 
-			{isOpen && <div className="h-[300px] border border-gray-200 border-t-0 bg-white p-5">내용</div>}
+			{isOpen && <div className="border border-gray-200 border-t-0 bg-white p-5">{children}</div>}
 		</div>
 	);
 }
--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,3 +1,4 @@
+import Input from "@/common/other/Input";
 import AccordionBox from "./AccordionBox";
 
 export default function Create() {
@@ -6,11 +7,36 @@ export default function Create() {
 			<section className="w-[390px] h-[1000px] border border-gray-200 rounded-lg bg-white">미리보기</section>
 
 			<section className="flex flex-col w-[580px] h-[2500px] rounded-sm bg-gray-50 gap-4">
+				{PERSON_INFO.map((person) => (
+					<AccordionBox title="신랑측 정보">
+						<div>
+							<div className="flex content-center items-center text-sm gap-2 mb-4">
+								<p className="mr-4">아버님</p>
+								<Input width="s" placeholder="성" />
+								<Input width="s" placeholder="이름" />
+							</div>
+
+							<div className="flex content-center items-center text-sm gap-2 mb-4">
+								<p className="mr-4">어머님</p>
+								<Input width="s" placeholder="성" />
+								<Input width="s" placeholder="이름" />
+							</div>
+
+							<div className="flex content-center items-center text-sm gap-2">
+								<p className="mr-4">{person}</p>
+								<Input width="s" placeholder="성" />
+								<Input width="s" placeholder="이름" />
+							</div>
+						</div>
+					</AccordionBox>
+				))}
+
+				{/* <AccordionBox />
 				<AccordionBox />
-				<AccordionBox />
-				<AccordionBox />
-				<AccordionBox />
+				<AccordionBox /> */}
 			</section>
 		</div>
 	);
 }
+
+const PERSON_INFO = ["신랑님", "신부님"];
