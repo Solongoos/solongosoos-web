592f6141f65bb44490b064d29de7caf13f6c4eb3
jezeony <jezeony@gmail.com>
2024-12-22 15:22:53 +0900

    feat : 예식일시 및 예식장소 입력란 추가

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -19,31 +19,64 @@ export default function Create() {
 					<AccordionBox title={idx === 0 ? "신랑측 정보" : "신부측 정보"}>
 						<div>
 							<div className="flex content-center items-center text-sm gap-2 mb-4">
-								<p className="mr-4 w-[50px]">아버님</p>
+								<p className="mr-4 w-[60px]">아버님</p>
 								<Input width="s" placeholder="성" />
 								<Input width="s" placeholder="이름" />
 							</div>
 
 							<div className="flex content-center items-center text-sm gap-2 mb-4">
-								<p className="mr-4  w-[50px]">어머님</p>
+								<p className="mr-4  w-[60px]">어머님</p>
 								<Input width="s" placeholder="성" />
 								<Input width="s" placeholder="이름" />
 							</div>
 
 							<div className="flex content-center items-center text-sm gap-2 mb-4">
-								<p className="mr-4  w-[50px]">{person}</p>
+								<p className="mr-4  w-[60px]">{person}</p>
 								<Input width="s" placeholder="성" />
 								<Input width="s" placeholder="이름" />
 							</div>
 
 							<div className="flex content-center items-center text-sm gap-2 mb-4">
-								<p className="mr-4  w-[50px]">관계</p>
-								<Dropdown option={OPTION} onSelect={handleSelect} selectedOption={selectedOption}/>
+								<p className="mr-4  w-[60px]">관계</p>
+								<Dropdown option={OPTION} onSelect={handleSelect} selectedOption={selectedOption} />
 							</div>
 						</div>
 					</AccordionBox>
 				))}
 
+				<AccordionBox title="예식 일시">
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[60px]">예식일</p>
+						<input className={`h-[40px] border border-gray-300 rounded p-[12px] text-xs `} type="date" />
+					</div>
+
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[60px]">예식시간</p>
+						<input className={`h-[40px] border border-gray-300 rounded p-[12px] text-xs `} type="time" />
+					</div>
+
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[60px]">표시</p>
+						체크박스
+					</div>
+				</AccordionBox>
+
+				<AccordionBox title="예식 장소">
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[60px]">예식작명</p>
+						<Input placeholder="예식장명을 입력해주세요" />
+					</div>
+
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[60px]">층과 홀</p>
+						<Input placeholder="층과 홀 이름을 입력해주세요" />
+					</div>
+
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[60px]">주소</p>
+						<Input placeholder="주소를 입력해주세요" />
+					</div>
+				</AccordionBox>
 			</section>
 		</div>
 	);
