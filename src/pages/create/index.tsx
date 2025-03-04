03b6c99c125842d3fbb412287acb7fc1a7d00e2b
jezeony <jezeony@gmail.com>
2025-03-04 17:38:15 +0900

    feat : 연락처 입력란 추가

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -95,6 +95,46 @@ export default function Create() {
 						<Input placeholder="주소를 입력해주세요" />
 					</div>
 				</AccordionBox>
+
+				<AccordionBox title="연락처">
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[90px]">신랑</p>
+						<Input placeholder="성함" width="m" />
+						<Input placeholder="전화번호" width="s" />
+					</div>
+
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[90px]">신랑 아버지</p>
+						<Input placeholder="성함" width="m" />
+						<Input placeholder="전화번호" width="s" />
+					</div>
+
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[90px]">신랑 어머니</p>
+						<Input placeholder="성함" width="m" />
+						<Input placeholder="전화번호" width="s" />
+					</div>
+
+					<hr className="my-6" />
+
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[90px]">신부</p>
+						<Input placeholder="성함" width="m" />
+						<Input placeholder="전화번호" width="s" />
+					</div>
+
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[90px]">신부 아버지</p>
+						<Input placeholder="성함" width="m" />
+						<Input placeholder="전화번호" width="s" />
+					</div>
+
+					<div className="flex content-center items-center text-sm gap-2 mb-4">
+						<p className="mr-4 w-[90px]">신부 어머니</p>
+						<Input placeholder="성함" width="m" />
+						<Input placeholder="전화번호" width="s" />
+					</div>
+				</AccordionBox>
 			</section>
 		</div>
 	);
